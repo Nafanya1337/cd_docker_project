b8feadb971b297583ef327c7e47422071bfa81d7
Фёдор Шмаков <shmakov987@mail.ru>
2026-05-03 00:17:24 +0300

    Initial CD Docker project

---
+++ test_app.py
@@ -0,0 +1,23 @@
+from app import app, get_message
+
+
+def test_home_status_code():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_home_message():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.data.decode("utf-8") == "Hello, Continuous Deployment!"
+
+
+def test_health_status_code():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
+def test_get_message():
+    assert get_message() == "Hello, Continuous Deployment!"
