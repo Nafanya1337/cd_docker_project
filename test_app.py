1d9c908f3e436b4ef65073411acea9891b46b08d
Фёдор Шмаков <shmakov987@mail.ru>
2026-05-03 03:07:13 +0300

    Update application message and tests

--- test_app.py
+++ test_app.py
@@ -10,7 +10,7 @@ def test_home_status_code():
 def test_home_message():
     client = app.test_client()
     response = client.get("/")
-    assert response.data.decode("utf-8") == "Hello, Continuous Deployment!"
+    assert response.data.decode("utf-8") == "Hello, Continuous Deployment v2!"
 
 
 def test_health_status_code():
@@ -20,4 +20,4 @@ def test_health_status_code():
 
 
 def test_get_message():
-    assert get_message() == "Hello, Continuous Deployment!"
+    assert get_message() == "Hello, Continuous Deployment v2!"
